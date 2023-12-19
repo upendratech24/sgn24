587fba8b0c6753bda4268c615cd4edb4ec65b335
Upendra Singh <upendratech24@gmail.com>
2023-12-19 20:18:01 +0530

    Hooks to be initiated

--- App.js
+++ App.js
@@ -1,31 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LoginMain from "./myComponent/Login_SignUp/LoginMain.js";
-import Login from "./myComponent/Login_SignUp/Login.js";
-import SignUp from "./myComponent/Login_SignUp/SignUp.js";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import LoginMain from "./myComponent/Pages/LoginMain.js";
+import Login from "./myComponent/Pages/Login.js";
+import SignUp from "./myComponent/Pages/SignUp.js";
 import User_Homepage from "./myComponent/Pages/User_Homepage.js";
+import Error from "./myComponent/Pages/Error.js";
 
 const App = () => {
-  return <div className="App_Container">{console.log("App Rendered...")}</div>;
+  return (
+    <div className="App_Container">
+      <Outlet />
+      {console.log("App Rendered...")}
+    </div>
+  );
 };
 
 const AppRouter = createBrowserRouter([
   {
     path: "/",
-    element: <LoginMain />,
-  },
-  {
-    path: "/accounts/emailsignup/",
-    element: <SignUp />,
-  },
-  {
-    path: "/accounts/login/",
-    element: <Login />,
-  },
-  {
-    path: "/userhomepage/",
-    element: <User_Homepage />,
+    element: <App />,
+    children: [
+      {
+        path: "/accounts/emailsignup/",
+        element: <SignUp />,
+      },
+      {
+        path: "/accounts/login/",
+        element: <Login />,
+      },
+      {
+        path: "/accounts/loginmain/",
+        element: <LoginMain />,
+      },
+      {
+        path: "/userhomepage/",
+        element: <User_Homepage />,
+      },
+    ],
+    errorElement: <Error />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
